5cbacd8f1bdd42f1587102d447d4ca5f6cdf5a02
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-26 01:02:09 +0200

    Correction bug RecupFichier

--- src/main/java/components/RecupFichier.java
+++ src/main/java/components/RecupFichier.java
@@ -1,8 +1,5 @@
 package components;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -16,13 +13,12 @@ import entities.Categorie;
 import entities.Ingredient;
 import entities.Allergene;
 
-
 public class RecupFichier {
 	public static HashMap<String, Categorie> categories;
 	public static HashMap<String, Marque> marques;
 	public static HashMap<String, Ingredient> ingredients;
 	public static HashMap<String, Allergene> allergenes;
-  
+
 	private RecupFichier() {
 		super();
 		RecupFichier.categories = new HashMap<>();
@@ -31,8 +27,7 @@ public class RecupFichier {
 		RecupFichier.allergenes = new HashMap<>();
 	}
 
-	public static RecupFichier getInstance() throws IOException
-	{
+	public static RecupFichier getInstance() throws IOException {
 		RecupFichier recupFichier = new RecupFichier();
 		Path path = Paths.get(".\\src\\main\\resources\\open-food-facts.csv");
 		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
@@ -46,24 +41,25 @@ public class RecupFichier {
 			}
 
 			Categorie categorie = categories.get(tokens[0]);
-			if(categorie == null) {
+			if (categorie == null) {
 				categorie = new Categorie(tokens[0]);
 				categories.put(tokens[0], categorie);
 			}
 
 			Marque marque = marques.get(tokens[1]);
-			if(marque == null) {
+			if (marque == null) {
 				marque = new Marque(tokens[1]);
 				marques.put(tokens[1], marque);
 			}
-			
+
 			ArrayList<Allergene> lstAllergenes = RecupFichier.initAllergenes(tokens[28]);
 
 			getIngredients(tokens[4]);
-		
+
+		}
 		return recupFichier;
 	}
-	
+
 	private static ArrayList<Allergene> initAllergenes(String lineAllergenes) {
 		lineAllergenes = lineAllergenes == null ? "" : lineAllergenes;
 		String[] allergenes = lineAllergenes.split(",");
@@ -71,29 +67,29 @@ public class RecupFichier {
 		for (String newAllergene : allergenes) {
 			newAllergene = newAllergene.toLowerCase().replace("fr:", "").replace("en:", "").replace("*", "").trim();
 			Allergene allergene = RecupFichier.allergenes.get(newAllergene);
-			if(allergene == null) {
+			if (allergene == null) {
 				allergene = new Allergene(newAllergene);
 				RecupFichier.allergenes.put(newAllergene, allergene);
 			}
 			lstAllergenes.add(allergene);
 		}
-		
+
 		return lstAllergenes;
+	}
 
-	public static void getIngredients(String lineIngredients){
-		String[]  arrIngredient = lineIngredients.replace("_","").trim().split("[,\\-]");
+	public static void getIngredients(String lineIngredients) {
+		String[] arrIngredient = lineIngredients.replace("_", "").trim().split("[,\\-]");
 
-		for(String unIngredient : arrIngredient){
+		for (String unIngredient : arrIngredient) {
 
-			unIngredient = unIngredient.replace(".","").trim().toLowerCase();
-			Ingredient newIngredient = ingredient.get(unIngredient);
+			unIngredient = unIngredient.replace(".", "").trim().toLowerCase();
+			Ingredient newIngredient = ingredients.get(unIngredient);
 
-			if(newIngredient == null) {
+			if (newIngredient == null) {
 				newIngredient = new Ingredient(unIngredient);
-				ingredient.put(unIngredient, newIngredient);
+				ingredients.put(unIngredient, newIngredient);
 			}
 
 		}
 	}
 }
-
