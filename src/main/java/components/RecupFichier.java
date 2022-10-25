79379e25d372c848705b1f1ba517ea1986bef069
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-26 00:43:51 +0200

    Initialisation des allergenes

--- src/main/java/components/RecupFichier.java
+++ src/main/java/components/RecupFichier.java
@@ -5,17 +5,22 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import entities.Allergene;
 import entities.Marque;
 
 public class RecupFichier {
 	public static HashMap<String, Marque> marques;
+	public static HashMap<String, Allergene> allergenes;
 	
 	private RecupFichier() {
 		super();
 		RecupFichier.marques = new HashMap<>();
+		RecupFichier.allergenes = new HashMap<>();
 	}
 	
 	public static RecupFichier getInstance() throws IOException
@@ -26,14 +31,37 @@ public class RecupFichier {
 		lines.remove(0);
 		
 		for (String line : lines) {
-			String[] tokens = line.replace("|", ";").split(";");
+
+			String[] tokens = new String[30];
+			for (int i = 0; i < line.split("\\|").length; i++) {
+				tokens[i] = line.split("\\|")[i];
+			}
 			Marque marque = marques.get(tokens[1]);
 			if(marque == null) {
 				marque = new Marque(tokens[1]);
 				marques.put(tokens[1], marque);
 			}
+			
+			ArrayList<Allergene> lstAllergenes = RecupFichier.initAllergenes(tokens[28]);
 		}
-
+		
 		return recupFichier;
+	}
+	
+	private static ArrayList<Allergene> initAllergenes(String lineAllergenes) {
+		lineAllergenes = lineAllergenes == null ? "" : lineAllergenes;
+		String[] allergenes = lineAllergenes.split(",");
+		ArrayList<Allergene> lstAllergenes = new ArrayList<>();
+		for (String newAllergene : allergenes) {
+			newAllergene = newAllergene.toLowerCase().replace("fr:", "").replace("en:", "").replace("*", "").trim();
+			Allergene allergene = RecupFichier.allergenes.get(newAllergene);
+			if(allergene == null) {
+				allergene = new Allergene(newAllergene);
+				RecupFichier.allergenes.put(newAllergene, allergene);
+			}
+			lstAllergenes.add(allergene);
+		}
+		
+		return lstAllergenes;
 	}
 }
