dd728e9650abf04b6963670c8e4f4b0374e04e59
Myriam <myriam@cinetwork.app>
2022-10-25 17:20:53 +0200

    add searchCategory for RecupoFiochier

--- src/main/java/components/RecupFichier.java
+++ src/main/java/components/RecupFichier.java
@@ -1,5 +1,8 @@
 package components;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -7,15 +10,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class RecupFichier {
+import entities.Categorie;
 
+public class RecupFichier {
+	public static Categorie categorie;
 	
-	public static List<String> recupData() throws IOException
+
+	public static void main(String[] args) throws IOException  
 	{
-	
+		RecupFichier recupFichier = new RecupFichier();
 		Path path = Paths.get(".\\src\\main\\data\\open-food-facts.csv");
 		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-		lines.remove(1);
-		return lines;
-	}
+		lines.remove(0);
+//		
+ for (int i = 0; i < lines.size(); i++) {
+     String replaceString=lines.get(i).replace("|",";"); 
+		String[] line = replaceString.split(";");
+
+  categorie = new Categorie(line[0]) ;
+  System.out.println(categorie.getLibelle());
+ 	}
+
+
+	
+
+ 
 }
+}
+ 
