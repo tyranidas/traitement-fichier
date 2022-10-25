29c3af71c8ed5bab26f30ab8628c70e12f00cd61
Myriam <myriam@cinetwork.app>
2022-10-25 14:34:00 +0200

    add allertgene class

---
+++ src/main/java/entities/Allegerne.java
@@ -0,0 +1,39 @@
+package entities;
+/**
+ * 
+ * @author MyriamAgag
+ * 
+ * Représente un allerge
+ *
+ */
+
+public class Allegerne {
+
+	/**
+	 * Nom de l'allergène
+	 */
+	
+	String libelle;
+
+	/**
+	 * 
+	 * @param libelle Nom de l'allergène
+	 *
+	 */
+	public Allegerne(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
+	
+	
+
+}
