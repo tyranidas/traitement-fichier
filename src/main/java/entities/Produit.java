7c1086636e88bc5187d06791df538685404a5a7e
Elshindr <headkeytics@gmail.com>
2022-10-25 15:28:26 +0200

    Ajout de la Classe Produit

--- src/main/java/entities/Produit.java
+++ src/main/java/entities/Produit.java
@@ -3,6 +3,9 @@ package entities;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The type Produit.
+ */
 public class Produit {
     private Categorie categorie;
     private Marque marque;
@@ -13,6 +16,17 @@ public class Produit {
     private List<Allergene> lstAllegerne;
 
 
+    /**
+     * Instantiates a new Produit.
+     *
+     * @param categorie         the categorie
+     * @param marque            the marque
+     * @param scoreNutritionnel the score nutritionnel
+     * @param lstAttributs      the lst attributs
+     * @param lstIngredient     the lst ingredient
+     * @param lstAdditif        the lst additif
+     * @param lstAllergene      the lst allergene
+     */
     public Produit(Categorie categorie, Marque marque, String scoreNutritionnel, Map<String, String> lstAttributs, List<Ingredient> lstIngredient, List<Additif> lstAdditif, List<Allergene> lstAllergene) {
         this.categorie = categorie;
         this.marque = marque;
@@ -23,50 +37,128 @@ public class Produit {
         this.lstAllegerne = lstAllergene;
     }
 
+    /**
+     * Gets categorie.
+     *
+     * @return the categorie
+     */
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    /**
+     * Sets categorie.
+     *
+     * @param categorie the categorie
+     */
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    /**
+     * Gets marque.
+     *
+     * @return the marque
+     */
     public Marque getMarque() {
         return marque;
     }
 
+    /**
+     * Sets marque.
+     *
+     * @param marque the marque
+     */
     public void setMarque(Marque marque) {
         this.marque = marque;
     }
 
+    /**
+     * Gets score nutritionnel.
+     *
+     * @return the score nutritionnel
+     */
     public String getScoreNutritionnel() {
         return scoreNutritionnel;
     }
 
+    /**
+     * Sets score nutritionnel.
+     *
+     * @param scoreNutritionnel the score nutritionnel
+     */
     public void setScoreNutritionnel(String scoreNutritionnel) {
         this.scoreNutritionnel = scoreNutritionnel;
     }
 
+    /**
+     * Gets lst attributs.
+     *
+     * @return the lst attributs
+     */
     public Map<String, String> getLstAttributs() {
         return lstAttributs;
     }
 
+    /**
+     * Sets lst attributs.
+     *
+     * @param lstAttributs the lst attributs
+     */
     public void setLstAttributs(Map<String, String> lstAttributs) {
         this.lstAttributs = lstAttributs;
     }
 
+    /**
+     * Gets lst ingredient.
+     *
+     * @return the lst ingredient
+     */
     public List<Ingredient> getLstIngredient() {
         return lstIngredient;
     }
 
+    /**
+     * Sets lst ingredient.
+     *
+     * @param lstIngredient the lst ingredient
+     */
     public void setLstIngredient(List<Ingredient> lstIngredient) {
         this.lstIngredient = lstIngredient;
     }
 
+    /**
+     * Gets lst additif.
+     *
+     * @return the lst additif
+     */
     public List<Additif> getLstAdditif() {
         return lstAdditif;
     }
 
+    /**
+     * Sets lst additif.
+     *
+     * @param lstAdditif the lst additif
+     */
     public void setLstAdditif(List<Additif> lstAdditif) {
         this.lstAdditif = lstAdditif;
     }
 
+    /**
+     * Gets lst allegerne.
+     *
+     * @return the lst allegerne
+     */
     public List<Allergene> getLstAllegerne() {
         return lstAllegerne;
     }
 
+    /**
+     * Sets lst allegerne.
+     *
+     * @param lstAllergene the lst allergene
+     */
     public void setLstAllegerne(List<Allergene> lstAllergene) {
         this.lstAllegerne = lstAllergene;
     }
