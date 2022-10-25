4d0ce4d49c4b87c2fab5801937577b959e11b088
Manuel <tyranidas@hotmail.com>
2022-10-25 16:20:28 +0200

    recupfichier sup 1ere ligne

--- src/main/java/components/RecupFichier.java
+++ src/main/java/components/RecupFichier.java
@@ -15,6 +15,7 @@ public class RecupFichier {
 	
 		Path path = Paths.get(".\\src\\main\\data\\open-food-facts.csv");
 		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+		lines.remove(1);
 		return lines;
 	}
 }
